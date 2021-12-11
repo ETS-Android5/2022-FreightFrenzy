afedd62fbd06892e82f473816709dafafbbc1f40
Robotics1 <roboticsndcl@gmail.com>
2021-12-11 13:47:30 -0500

    Reversed wheel positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyBlueAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyBlueAutonomous.java
@@ -191,10 +191,10 @@ public class FreightFrenzyBlueAutonomous extends LinearOpMode {
         duckMotor = hardwareMap.dcMotor.get("duckMotor");
 
         //setting the direction for each motor
-        frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         duckMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
