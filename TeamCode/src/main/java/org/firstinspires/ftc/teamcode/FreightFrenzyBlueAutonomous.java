d7666b99f73fef6ee68c0a8fc7a8c8576b434ecb
Robotics1 <roboticsndcl@gmail.com>
2021-12-15 14:23:05 -0500

    Implemented more steps

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyBlueAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyBlueAutonomous.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2019 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import java.util.ArrayList;
@@ -34,6 +5,7 @@ import java.util.List;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
@@ -65,8 +37,14 @@ public class FreightFrenzyBlueAutonomous extends LinearOpMode {
     private DcMotor frontRightMotor = null;
     private DcMotor backLeftMotor = null;
     private DcMotor backRightMotor = null;
+    private CRServo extendServo = null;
 
     private DcMotor duckMotor = null;
+    private DcMotor liftMotor = null;
+
+    private int liftMotorPos;
+    private int liftMotorZero;
+    private int liftMotorMax;
 
     private int drive1 = 250; // modify this based on optimal battery speed
     private int stop1 = drive1 + 100;
@@ -75,6 +53,8 @@ public class FreightFrenzyBlueAutonomous extends LinearOpMode {
     private int duck1 = drive2 + 3200;
     private int pan2 = duck1 + 1250;
     private int drive3 = pan2 + 750;
+    private int pan3 = drive3 + 1500;
+    private int drive4 = pan3 + 1000;
 
     public void drive (String fb, double speedMod)
     {
@@ -167,6 +147,16 @@ public class FreightFrenzyBlueAutonomous extends LinearOpMode {
         duckMotor.setPower(duckPower);
     }
 
+    public void lift (Boolean uber)
+    {
+        if (uber)
+        {
+            liftMotor.setPower(1.0);
+        }
+        else
+            liftMotor.setPower(0.0);
+    }
+
     ////////////////////////////////////////////////////////////////////////////////////////////////
     private static final String TFOD_MODEL_ASSET = "amogus.tflite";
     private static final String[] LABELS =
@@ -197,6 +187,7 @@ public class FreightFrenzyBlueAutonomous extends LinearOpMode {
         backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
         duckMotor = hardwareMap.dcMotor.get("duckMotor");
+        liftMotor = hardwareMap.dcMotor.get("liftMotor");
 
         //setting the direction for each motor
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -204,6 +195,7 @@ public class FreightFrenzyBlueAutonomous extends LinearOpMode {
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         duckMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        liftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
         // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
         // first.
@@ -233,6 +225,7 @@ public class FreightFrenzyBlueAutonomous extends LinearOpMode {
 
         if (opModeIsActive()) {
             long initTime = System.currentTimeMillis();
+            liftMotorZero = liftMotor.getCurrentPosition();
             while (opModeIsActive()) {
                 if (tfod != null) {
                     // getUpdatedRecognitions() will return null if no new information is available since
@@ -259,6 +252,7 @@ public class FreightFrenzyBlueAutonomous extends LinearOpMode {
                         long finalTime = System.currentTimeMillis() - initTime;
                         long timeDifference = 0;
                         loopCount += 1;
+                        liftMotorPos = liftMotor.getCurrentPosition() - liftMotorZero;
 
                         telemetry.addData("Loop count:", loopCount);
                         if (resetTime == 0)
@@ -266,12 +260,13 @@ public class FreightFrenzyBlueAutonomous extends LinearOpMode {
                             telemetry.addData("Time is:", finalTime);
                             telemetry.addData("Ms/loop:", finalTime / loopCount);
                         }
-                        if (resetTime == 1)
+                        if (resetTime > 0)
                         {
                             telemetry.addData("Time is:", finalTime + timeDifference);
                             telemetry.addData("Ms/loop:", (finalTime + timeDifference) / loopCount);
                         }
                         telemetry.addData("Element position: ", elementPosition);
+                        telemetry.addData("Lift motor position: ", liftMotorPos);
 
                         // initial step - detect what position duck/team element is in
                         if (finalTime > 1500 && !elementFound)
@@ -330,8 +325,23 @@ public class FreightFrenzyBlueAutonomous extends LinearOpMode {
                             {
                                 drive(BACKWARD, 0.3);
                             }
-                            if (finalTime > drive3)
+                            if (finalTime < pan3 & finalTime > drive3)
                             {
+                                pan(LEFT);
+                                if ((elementPosition == 2 & liftMotorPos <= 2300) | (elementPosition == 3 & liftMotorPos <= 6000))
+                                {
+                                    lift(ON);
+                                }
+                                else
+                                {
+                                    lift(OFF);
+                                }
+                            }
+                            if (finalTime < drive4 & finalTime > pan3)
+                            {
+                                drive(FORWARD, 0.3);
+                            }
+                            if (finalTime > drive4) {
                                 drive(STOP, 0.0);
                             }
                         }
